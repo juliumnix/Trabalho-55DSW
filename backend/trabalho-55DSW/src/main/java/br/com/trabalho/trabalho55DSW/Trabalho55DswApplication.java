7e765c4303c4aef377706e898d810f1ccdbd0a7c
AnaaBananaa <ana.reinert@edu.udesc.br>
2023-02-16 21:46:34 -0300

    feat: Adição de projeto maven e configuração inicial de postgres
    Criação do projeto maven com as devidas dependencias do spring boot e
    postgreSQL já adicionadas ao pom,xml. Adição de configurações iniciais
    do postgres no arquivo de application

---
+++ backend/trabalho-55DSW/src/main/java/br/com/trabalho/trabalho55DSW/Trabalho55DswApplication.java
@@ -0,0 +1,13 @@
+package br.com.trabalho.trabalho55DSW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho55DswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalho55DswApplication.class, args);
+	}
+
+}
---
+++ backend/trabalho-55DSW/src/test/java/br/com/trabalho/trabalho55DSW/Trabalho55DswApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trabalho.trabalho55DSW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho55DswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
