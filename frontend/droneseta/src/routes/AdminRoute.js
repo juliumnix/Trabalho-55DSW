09a81c7acd69a45bfada78fbc33a72a067b8b604
AnaaBananaa <ana.reinert@edu.udesc.br>
2023-02-22 22:00:47 -0300

    feat: Nova rota de admin

---
+++ frontend/droneseta/src/routes/AdminRoute.js
@@ -0,0 +1,11 @@
+import { Navigate } from 'react-router-dom';
+
+export default function AdminRoute({ children }) {
+    console.log(localStorage.getItem("authLoginAdmin"))
+    if (localStorage.getItem("authLoginAdmin") === true) {
+        console.log(localStorage.getItem("authLoginAdmin"))
+        return <Navigate to='/admin' />
+    } else {
+        return children
+    }
+}
