f63a5a6121a83446e95f8940275607fd0fea283b
juliumnix <juliumnix@gmail.com>
2023-03-09 23:47:47 -0300

    criando pagina de erro

--- frontend/droneseta/src/pages/ErrorScreen/index.js
+++ frontend/droneseta/src/pages/ErrorScreen/index.js
@@ -1,9 +1,84 @@
-import React from 'react';
+import React from "react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import LogoutIcon from "@mui/icons-material/Logout";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-import { Container } from './styles.js';
+import {
+  Container,
+  ContainerError,
+  StatusMessage,
+  Subtitle,
+  Title,
+} from "./styles.js";
+import {
+  ClickMenu,
+  ItemButton,
+  Logo,
+  Spacer,
+} from "../../components/Header/styles";
+import { useUsuario } from "../../hooks/UsuarioHook.js";
+import Header from "../../components/Header/index.js";
+import { useNavigate } from "react-router-dom";
 
 function ErrorScreen() {
-  return <Container />;
+  const navigate = useNavigate();
+  const { clearUsuarioFromLocalState } = useUsuario();
+  function logout() {
+    clearUsuarioFromLocalState();
+    navigate("/");
+  }
+  return (
+    <Container>
+      <Header
+        leftChildren={
+          <>
+            <Spacer />
+            <Logo
+              src={require("../../assets/logo.png")}
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                navigate("/home");
+              }}
+            />
+            <Spacer />
+            <ItemButton
+              onClick={() => {
+                navigate("/products");
+              }}
+            >
+              PRODUTOS
+            </ItemButton>
+          </>
+        }
+        rightChildren={
+          <>
+            <ClickMenu
+              onClick={() => {
+                navigate("/shopping-cart");
+              }}
+            >
+              <ShoppingCartIcon
+                style={{ fontSize: "4vh", cursor: "pointer" }}
+              />
+            </ClickMenu>
+
+            <Spacer />
+            <ClickMenu onClick={logout}>
+              <LogoutIcon style={{ fontSize: "4vh", cursor: "pointer" }} />
+            </ClickMenu>
+            <Spacer />
+          </>
+        }
+      />
+      <ContainerError>
+        <Title>Ops...</Title>
+        <Subtitle>
+          Parece que a página que você quer acessar não existe...
+        </Subtitle>
+        <StatusMessage>404</StatusMessage>
+      </ContainerError>
+    </Container>
+  );
 }
 
 export default ErrorScreen;
