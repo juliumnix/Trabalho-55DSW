eb08546c30d1332673a93101f9afb8c367588f2c
juliumnix <juliumnix@gmail.com>
2023-04-07 17:52:01 -0300

    melhorando sistema de path

--- backend/droneseta/src/main/java/br/com/trabalho/droneseta/utils/EnvironmentPath.java
+++ backend/droneseta/src/main/java/br/com/trabalho/droneseta/utils/EnvironmentPath.java
@@ -1,15 +1,21 @@
 package br.com.trabalho.droneseta.utils;
 
 public enum EnvironmentPath {
-    PATH("D:\\workspace\\projeto-55DSWF\\Trabalho-55DSW\\backend\\droneseta\\src\\main\\resources\\imagens");
+    PATH("");
 
     private String path;
 
     EnvironmentPath(String path) {
         this.path = path;
+        String currentDir = System.getProperty("user.dir");
+        setPath(currentDir+"\\src\\main\\resources\\imagens");
     }
 
     public String getPath() {
         return path;
+    }
+
+    private void setPath(String novoPath) {
+        path = novoPath;
     }
 }
